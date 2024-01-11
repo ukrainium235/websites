6d5e32e52a375fec9135613340c926401ec6297e
gv <ukrainium238@gmail.com>
2024-01-10 20:04:28 -0500

    First class after trip. Talked about bitburner. The changes made are implementing animations to move the balls in a certain ease-in-out way

--- website-1/tailwind.config.js
+++ website-1/tailwind.config.js
@@ -4,10 +4,11 @@ module.exports = {
   theme: {
     extend: {
       keyframes:{
-      devurandomkey:{
-        ;                                                                                                     
-      }
-      },
+        devurandomkey: {
+          '0%, 100%': { transform: 'translate(0)' },
+          '50%': { transform: 'translate(1vmin,0)' },
+        }
+      },           
       animation:{
         devurandom: "devurandomkey ease-in-out infinite 4s  "
 
